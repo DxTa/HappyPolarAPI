116a480cb491812d917c39cd77487b229870b69c
Minh Duc Ta <ductm310@gmail.com>
2016-03-27 10:44:19 +0300

    fix allowOrigin

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ var allowCrossDomain = function(req, res, next) {
             res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
             res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Origin, Accept');
             next();
+        } else {
+          next();
         }
 };
 app.use(allowCrossDomain);
